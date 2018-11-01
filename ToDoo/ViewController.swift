8c1303cd5e66e2c2d38ef215fca8a2cf21391e2f
Razvan Armeanu <razvan91@gmail.com>
2018-11-01 21:21:15 +0200

    Initial Commit

---
+++ ToDoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoo
+//
+//  Created by Razvan Armeanu on 01/11/2018.
+//  Copyright © 2018 Razz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
